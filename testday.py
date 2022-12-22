1d2bc1d9d7093d837e54ee3466b623cf449f6a85
wesoree <wesleylo174@gmail.com>
2022-12-22 15:28:55 -0500

    Update testday.py

--- testday.py
+++ testday.py
@@ -6,6 +6,8 @@ print('hello world')
 
 input_list = aocday(DAY, True, False)
 
+input_list2 = aocday(DAY, True, None)
+
 input_lines = aocday(DAY, False, True)
 
 input_data = aocday(DAY, False, False)
@@ -16,3 +18,5 @@ print(input_list)
 print(input_lines)
 
 print(input_data)
+
+print(input_list2)
